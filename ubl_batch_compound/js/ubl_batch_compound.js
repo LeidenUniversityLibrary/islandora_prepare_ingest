4a3f7a98737895c18e14d330de84d94c68cefa23
lucasvanschaik <schaik@library.leidenuniv.nl>
2016-09-21 13:53:11 +0200

    Handle parent creation better
    - remember parent selection while reviewing
    - let user choose if found parent is to be used
    - solved bug in checking if found parent should be used as compound

---
+++ ubl_batch_compound/js/ubl_batch_compound.js
@@ -0,0 +1,16 @@
+(function ($) {
+  Drupal.behaviors.ublBatchCompound = {
+    attach: function (context, settings) {
+      $('.viewlinkclass', context).click(function (e) {
+        var redirecturlfield = $('INPUT[name="redirecturl"]');
+        var newvalue = this.href;
+        if (newvalue) {
+          e.preventDefault(); 
+          redirecturlfield.val(newvalue);
+          $('#ubl-batch-compound-import-confirm-form').submit();
+        }
+      });
+    }
+  };
+})(jQuery);
+
